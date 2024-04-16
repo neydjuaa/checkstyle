adc4218e9cfc2ca1d79450f5ad691ebd487c51da
muharrem <marslandevops@gmail.com>
2024-04-16 14:33:28 +0200

    modifie sample.java

--- src/com/muthu4all/checkstyle/Sample.java
+++ src/com/muthu4all/checkstyle/Sample.java
@@ -2,7 +2,7 @@ package com.muthu4all.checkstyle;
 
 public class Sample {
 	/**
-        * This is sample method.
+            *This is sample method
   	*/
 	public Boolean shouldBeWarning() {
 		return null;
